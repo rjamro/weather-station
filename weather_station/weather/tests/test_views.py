312a98eab955c01f520dee0a46acc0318e6fd744
Rafal Jamro <r.jamro@gmail.pl>
2020-02-01 17:04:43 +0100

    added navigation between pages

--- weather_station/weather/tests/test_views.py
+++ weather_station/weather/tests/test_views.py
@@ -2,28 +2,9 @@ from django.test import TestCase
 from django.urls import reverse
 
 class WeatherViewTest(TestCase):
+    # weather view
     def test_weatherview_url_exists_at_desired_location(self):
         response = self.client.get('/weather/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_queryview_url_exists_at_desired_location(self):
-        response = self.client.get('/weather/query/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_coordinatesview_url_exists_at_desired_location(self):
-        response = self.client.get('/weather/coordinates/')
-        self.assertEqual(response.status_code, 200)
-
-    def test_forecastview_url_exists_at_desired_location(self):
-        response = self.client.get('/weather/forecast/1')
-        self.assertEqual(response.status_code, 200)
-
-    def test_queryview_is_accessible_by_name(self):
-        response = self.client.get(reverse('query_view'))
-        self.assertEqual(response.status_code, 200)
-
-    def test_coordinatesview_is_accessible_by_name(self):
-        response = self.client.get(reverse('coordinate_view'))
         self.assertEqual(response.status_code, 200)
 
     def test_weatherview_uses_correct_template(self):
@@ -31,15 +12,38 @@ class WeatherViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'forecast_form.html')
 
+    # query view
+    def test_queryview_url_exists_at_desired_location(self):
+        response = self.client.get('/weather/query/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_queryview_is_accessible_by_name(self):
+        response = self.client.get(reverse('query_view'))
+        self.assertEqual(response.status_code, 200)
+
     def test_queryview_uses_correct_template(self):
         response = self.client.get('/weather/query/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'forecast_cities.html')
 
+    # coordinate view
+    def test_coordinatesview_url_exists_at_desired_location(self):
+        response = self.client.get('/weather/coordinates/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_coordinatesview_is_accessible_by_name(self):
+        response = self.client.get(reverse('coordinate_view'))
+        self.assertEqual(response.status_code, 200)
+
     def test_coordinatesview_uses_correct_template(self):
         response = self.client.get('/weather/coordinates/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'forecast_cities.html')
+
+    # forecast view
+    def test_forecastview_url_exists_at_desired_location(self):
+        response = self.client.get('/weather/forecast/1')
+        self.assertEqual(response.status_code, 200)
 
     def test_forecastview_uses_correct_template(self):
         response = self.client.get('/weather/forecast/1')
--- weather_station/weather/views.py
+++ weather_station/weather/views.py
@@ -13,20 +13,38 @@ def get_forecast_detail(request):
     return render(request, "forecast_detail.html", context=context)
 
 def get_forecast_search(request):
-    coordinate_form = CoordinateForm()
-    city_form = CityForm()
-    context = {
-        "city_form": city_form,
-        "coordinate_form": coordinate_form
-    }
+    if request.method == 'POST':
+        if 'city_form' in request.POST:
+            city_form = CityForm(request.POST)
+            if city_form.is_valid():
+                query = city_form.cleaned_data["query"]
+                url = f'{reverse("query_view")}?text={query}'
+                return redirect(url)
+        elif 'coordinate_form' in request.POST:
+            coordinate_form = CoordinateForm(request.POST)
+            if coordinate_form.is_valid():
+                longitude = coordinate_form.cleaned_data["longitude"]
+                lattitude = coordinate_form.cleaned_data["lattitude"]
+                url = f'{reverse("coordinate_view")}?long={longitude}&latt={lattitude}'
+                return redirect(url)
+    else:
+        coordinate_form = CoordinateForm()
+        city_form = CityForm()
+        context = {
+            "city_form": city_form,
+            "coordinate_form": coordinate_form
+        }
 
-    return render(request, "forecast_form.html", context=context)
+        return render(request, "forecast_form.html", context=context)
 
 def get_cities_by_coordinate(request):
+    lattitude = request.GET.get("lattitude", 0.0)
+    longitude = request.GET.get("longitude", 0.0)
     context = {}
     return render(request, 'forecast_cities.html', context=context)
 
 def get_cities_by_query(request):
+    query = request.GET.get("query", "Not specified")
     context = {}
     return render(request, 'forecast_cities.html', context=context)
 
